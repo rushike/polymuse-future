50483f1e4a9181994291e456e51ea19010f0b6b7
Rushike Supreme <rushike.ab1@gmail.com>
2019-10-04 15:37:48 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="polymuse-future",
+    version="0.0.1",
+    # scripts = ['MIDI.py', 'mutils.py', 'rmidi.py', 'sound.py'],
+    packages=['polymuse', 'midis'],
+    author="rushike",
+    author_email="rushike.ab1@gmail.com",
+    description="Polymuse",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rushike/polymuse-future",
+    install_requires=['numpy', 'magenta', 'keras', 'tensorflow'],
+    # packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
